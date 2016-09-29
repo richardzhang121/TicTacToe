c0d04de46317d9dc782a86bbbfa57fa770de50d8
Richard Zhang <269006@beaverton.k12.or.us>
2016-09-29 14:18:36 -0700

    First Commit

---
+++ ZhangTicTacToe.cpp
@@ -0,0 +1,9 @@
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int main() {
+  char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+  cout << board << endl;
+  return 0;
+}
